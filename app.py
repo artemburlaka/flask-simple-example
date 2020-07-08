e198fa3f0042fb7a3452f2a02a13c63c236a6173
Artem Burlaka <guestartem@gmail.com>
2020-07-08 17:07:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+application = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@application.route("/")
+def root():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
